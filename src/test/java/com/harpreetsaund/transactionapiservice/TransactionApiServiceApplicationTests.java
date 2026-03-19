fc889997c47a07e0baddf84f519747821fdfe2f0
Harpreet Saund <saundharpreet99@gmail.com>
2026-03-19 11:20:21 -0400

    spotless

--- src/test/java/com/harpreetsaund/transactionapiservice/TransactionApiServiceApplicationTests.java
+++ src/test/java/com/harpreetsaund/transactionapiservice/TransactionApiServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.harpreetsaund.transactionapiservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransactionApiServiceApplicationTests {
 
     @Test
